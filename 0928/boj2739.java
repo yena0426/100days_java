3c9b318ab8d3cf5824a754a5a3ef3a240621c6f2
yena0426 <yena0426@gmail.com>
2022-09-29 09:12:33 +0900

    day006

---
+++ 0928/boj2739.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class boj2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.printf("%d * %d = %d\n", num, i, num*i);
+		}
+	}
+
+}
