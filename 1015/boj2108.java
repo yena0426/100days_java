bb98e0201a2bcc6bc36b3869fe0c2e44fc11dd22
yena0426 <yena0426@gmail.com>
2022-10-20 23:41:02 +0900

    028 1020

--- 1015/boj2108.java
+++ 1015/boj2108.java
@@ -1,20 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-//산술평균 : N개의 수들의 합을 N으로 나눈 값
-//중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-//최빈값 : N개의 수들 중 가장 많이 나타나는 값
-//범위 : N개의 수들 중 최댓값과 최솟값의 차이
-
+// arithmetic mean 산술평균 : N개의 수들의 합을 N으로 나눈 값
+// median 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+// mode 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+// range 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 
 public class boj2108 {
 	
 	public static int func1(int[] lst) {
-		int sum = 0;
+		double sum = 0;
 		for (int l : lst) {
 			sum += l;
 		}
-		int avg = sum/lst.length;
+		int avg = (int)Math.round(sum/lst.length);
 		return avg;
 	}
 	
@@ -28,8 +27,20 @@ public class boj2108 {
 		int maxcnt = 0;
 		for (int l : lst) {
 			cnt[l+4000] += 1;
-			maxcnt = 
+			maxcnt = (cnt[l+4000] > maxcnt) ? cnt[l+4000] : maxcnt;
 		}
+		int cntorder = 0;
+		int res = 0;
+		for (int i = 0; i < 8001; i++) {
+			if (cnt[i] == maxcnt) {
+				cntorder += 1;
+				res = i-4000;
+			}
+			if (cntorder == 2) {
+				break;
+			}
+		}
+		return res;
 	}
 	
 	public static int func4(int[] lst) {
@@ -52,7 +63,10 @@ public class boj2108 {
 			nums[i] = Integer.parseInt(br.readLine());
 		}
 				
-		sb.append(func1(nums)).append(func2(nums)).append(func3(nums)).append(func4(nums)).append("\n");
+		sb.append(func1(nums)).append("\n");
+		sb.append(func2(nums)).append("\n");
+		sb.append(func3(nums)).append("\n");
+		sb.append(func4(nums)).append("\n");
 		System.out.println(sb);
 	}
 	
